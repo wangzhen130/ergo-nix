39d62f296c54e67cb4c91ef61b6501ca10f009fa
王振 <wang.zhen@upex.co>
2022-11-03 11:01:05 +0800

    node 5.0.2

--- nixpkgs/ergo-node/default.nix
+++ nixpkgs/ergo-node/default.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "ergo-node";
-  version = "5.0.1";
+  version = "5.0.2";
 
   src = fetchurl {
     url = "https://github.com/ergoplatform/ergo/releases/download/v${version}/ergo-${version}.jar";
-    sha256 = "6a81f69d11acda6df85c3b496f3212f10f3074855de1277284ea4c6da9d3d49f";
+    sha256 = "93b11058e2d204f3eaf8062fb84492b94cdebff26d5a00ac65c4c614db9cb707";
   };
 
   nativeBuildInputs = [ makeWrapper ];
